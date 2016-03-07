71ec110764239c65358781193b6b602871761200
Binh Mai <binhvinhmai@gmail.com>
2016-03-07 17:58:12 -0600

    Added notes for 3-7-2016

---
+++ CS303 Notes and Code/3-7-2016 Notes.hpp
@@ -0,0 +1,34 @@
+//
+//  3-7-2016 Notes.hpp
+//  CS303 Notes and Code
+//
+//  Created by Binh Vinh Mai on 3/7/16.
+//  Copyright © 2016 Binh Vinh Mai. All rights reserved.
+//
+
+#ifndef __7_2016_Notes_hpp
+#define __7_2016_Notes_hpp
+
+#include <stdio.h>
+
+/* The OddEvenSort is really designed to run on two different servers - one would be handling the odd and the other would be handling the even files.
+ */
+void OddEvenSort()
+{
+    bool sorted = false
+    while (!sorted)
+    {
+        sorted = true;
+        for (int i = 1; i < a.size() - 1; i+=2)
+        {
+            // Code will need to pasted in here.
+        }
+        
+    }
+}
+
+
+/* We have been working on linear data structures. They're usually slow to insert/delete or access. 
+ */
+
+#endif /* __7_2016_Notes_hpp */
