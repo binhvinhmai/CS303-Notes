fb158eb17083ad235de65245141e5dd3f57a3fa4
Binh Mai <binhvinhmai@gmail.com>
2016-03-07 17:51:32 -0600

    Initial Commit

---
+++ CS303 Notes and Code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS303 Notes and Code
+//
+//  Created by Binh Vinh Mai on 3/7/16.
+//  Copyright © 2016 Binh Vinh Mai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
