b14889ffbf21836bbc4a1498c8500cbd88e78580
Binh Mai <binhvinhmai@gmail.com>
2016-03-14 17:51:31 -0500

    Added notes for 3-14-2016 Notes

---
+++ CS303 Notes and Code/3-14-2016 Notes.h
@@ -0,0 +1,29 @@
+//
+//  3-14-2016 Notes.h
+//  CS303 Notes and Code
+//
+//  Created by Binh Vinh Mai on 3/14/16.
+//  Copyright © 2016 Binh Vinh Mai. All rights reserved.
+//
+
+#ifndef __14_2016_Notes_h
+#define __14_2016_Notes_h
+
+/*
+ Data Structures - Midterm Exam Guide
+ 15 Multiple Choice Questions
+ 3 questions on Linked Lists, stacks, queues, recursion
+ 2 programming questions - write code here
+ Last question is a pseudo-code question algorithm. 
+ 
+ Look at the summaries at the end of each chapter. Don't spend a lot of time on the multiple-choice questions. 
+ Sorting is part of the multiple-choice question
+ Programming will mainly be recursion. 
+ 
+ Make sure that pseudocode has programming elements with coding elements. 
+ A question on what data structure you would use for some requirements. 
+ 
+ */
+
+
+#endif /* __14_2016_Notes_h */
