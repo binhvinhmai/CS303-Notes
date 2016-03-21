882b4d54d8dc5dd80193afff6687f0c14ad8d9d8
Binh Mai <binhvinhmai@gmail.com>
2016-03-21 18:37:33 -0500

    Typed a little bit more notes

--- CS303 Notes and Code/3-21-2016 Notes.h
+++ CS303 Notes and Code/3-21-2016 Notes.h
@@ -28,5 +28,17 @@ void startsWith(BTNode<Item_Type>* local_root, const Item_Type&target, list<Item
 
 // Writing an index for a term paper
         
+/*
+ A max heap orders its nodes in some descending order
+ Must be complete (each level except for the last level is filled, and all of the gaps are filled in on the left side).
+ Duplicates are allowed
+ 
+ In a min heap, the smallest value is the root.
+ 
+ Heaps can be used as priority queues. You can pop the smallest/lowest priority. It is O(1) to pop and push items. Sorting would not be more than O(nlogn). Insert the item to the next position across the bottom of the complete tree to preserve complementess. 
+ Find the values. 
+ 
+ I 
+ */
 
 #endif /* __21_2016_Notes_h */
